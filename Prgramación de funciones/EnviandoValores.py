281b63e22f1ed29c9de07d330fd2c2198742b4d8
Jesus Villalobos <jesusvillalobos2807@outlook.es>
2020-02-27 19:23:11 -0700

    Functions programming

---
+++ Prgramación de funciones/EnviandoValores.py
@@ -0,0 +1,14 @@
+# {************************************}
+#    Programa:      Enviando valores
+#    Autor:         Villalobos Valenzuela Jesús Héctor
+#    Fecha:         23/02/2020
+#    Descripción:   Entradas de datos.
+# {************************************}
+
+#  Funciones
+#{************************************}
+#    Función:       suma
+#    Autor:         Villalobos Valenzuela Jesús Héctor
+#    Fecha:         26/02/2020
+#    Desprición:    Está función resive dos números de entrada y lo suma.
+#{************************************}
